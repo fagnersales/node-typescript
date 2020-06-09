53ceab5d9250dfa56f17ea3f6de11c8b0507c062
Fagner Sales <fs.fagnersales@gmail.com>
2020-06-09 14:26:23 -0300

    Completed NodeTS using Express & Mongoose

---
+++ src/app.ts
@@ -0,0 +1,39 @@
+import express from 'express'
+import cors from 'cors'
+import mongoose from 'mongoose'
+
+import routes from './routes'
+
+const mongooseURI = 'mongodb+srv://fagner_sales:2f0a1g1n2e5r12@node-rest-tutorial-geqjm.gcp.mongodb.net/tsnode?retryWrites=true&w=majority'
+
+class App {
+    public express: express.Application
+
+    public constructor() {
+        this.express = express()
+
+        this.middlewares()
+        this.database()
+        this.routes()
+    }
+
+    private middlewares(): void {
+        this.express.use(express.json())
+        this.express.use(cors())
+    }
+
+    private database(): void {
+        mongoose.connect(mongooseURI, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+            useFindAndModify: true
+        }, () => console.log('[DATABASE] Connected'))
+    }
+
+    private routes(): void {
+        this.express.use(routes)
+    }
+
+}
+
+export default new App().express
